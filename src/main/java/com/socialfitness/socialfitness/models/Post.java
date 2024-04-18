7d5959adf54a0b10c024beed802c4d948335281d
ProjectsAssesment <kumaramaduka5@gmail.com>
2024-04-18 20:29:23 +0530

    create post model class

--- src/main/java/com/socialfitness/socialfitness/models/Post.java
+++ src/main/java/com/socialfitness/socialfitness/models/Post.java
@@ -1,2 +1,90 @@
-package com.socialfitness.socialfitness.models;public class Post {
+package com.socialfitness.socialfitness.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String caption;
+    private String image;
+    private String video;
+    private User user;
+    private LocalDateTime createdAt;
+    private List<User> liked = new ArrayList<>();
+    public Post() {
+
+    }
+
+    public Post(Integer id, String caption, String image, String video, User user, LocalDateTime createdAt, List<User> liked) {
+        this.id = id;
+        this.caption = caption;
+        this.image = image;
+        this.video = video;
+        this.user = user;
+        this.createdAt = createdAt;
+        this.liked = liked;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<User> getLiked() {
+        return liked;
+    }
+
+    public void setLiked(List<User> liked) {
+        this.liked = liked;
+    }
 }
