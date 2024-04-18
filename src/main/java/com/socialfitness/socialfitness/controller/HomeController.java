18bc72b792a948f45d43a63334dc0f65e902b66d
ProjectsAssesment <kumaramaduka5@gmail.com>
2024-04-18 12:05:21 +0530

    success test api

--- src/main/java/com/socialfitness/socialfitness/controller/HomeController.java
+++ src/main/java/com/socialfitness/socialfitness/controller/HomeController.java
@@ -1,2 +1,13 @@
-package com.socialfitness.socialfitness.controller;public class HomeController {
+package com.socialfitness.socialfitness.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String homeControllerHandler(){
+        return "Hello World!";
+    }
 }
