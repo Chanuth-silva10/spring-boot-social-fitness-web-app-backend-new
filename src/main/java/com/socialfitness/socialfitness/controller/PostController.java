7466bd48d241fb462188d395a42399ccaa478517
ProjectsAssesment <kumaramaduka5@gmail.com>
2024-04-18 20:29:06 +0530

    implement post controler

--- src/main/java/com/socialfitness/socialfitness/controller/PostController.java
+++ src/main/java/com/socialfitness/socialfitness/controller/PostController.java
@@ -1,2 +1,31 @@
-package com.socialfitness.socialfitness.controller;public class PostController {
+package com.socialfitness.socialfitness.controller;
+
+import com.socialfitness.socialfitness.models.Post;
+import com.socialfitness.socialfitness.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    PostService postService;
+
+    @PostMapping("/posts/user/{userId}")
+    public ResponseEntity<Post> createPost(@RequestBody Post post,@PathVariable Integer userId) throws Exception {
+
+        Post createdPost = postService.createNewPost(post,userId);
+
+        return new ResponseEntity<>(createdPost, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/posts/{postId}/user/{userId}")
+    public ResponseEntity<String> deletePost(@PathVariable Integer postId,@PathVariable Integer userId) throws Exception {
+
+        String message = postService.deletePost(postId,userId);
+
+        return null;
+    }
 }
