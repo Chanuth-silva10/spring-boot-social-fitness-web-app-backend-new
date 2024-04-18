fe84d2d983d083998489c9e5167a776c7f27b01a
ProjectsAssesment <kumaramaduka5@gmail.com>
2024-04-18 11:26:22 +0530

    spring initilizer

---
+++ src/main/java/com/socialfitness/socialfitness/SocialFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.socialfitness.socialfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialFitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialfitness/socialfitness/SocialFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialfitness.socialfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
