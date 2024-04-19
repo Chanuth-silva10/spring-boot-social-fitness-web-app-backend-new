600248186dcd20088ac87cda49d9c401d76746de
ProjectsAssesment <kumaramaduka5@gmail.com>
2024-04-19 19:33:09 +0530

    implement comment section

---
+++ src/main/java/com/socialfitness/socialfitness/service/CommentService.java
@@ -0,0 +1,14 @@
+package com.socialfitness.socialfitness.service;
+
+import com.socialfitness.socialfitness.models.Comment;
+
+public interface CommentService {
+
+    public Comment createComment(
+            Comment comment,
+            Integer postId,
+            Integer userId
+    ) throws Exception;
+    public Comment findCommentById(Integer commentId) throws Exception;
+    public Comment likeComment(Integer commentId, Integer userId) throws Exception;
+}
