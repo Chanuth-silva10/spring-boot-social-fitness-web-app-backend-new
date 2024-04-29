d563d73a8c55d2ce90a9da4863263b4e49f2cbc4
ProjectsAssesment <kumaramaduka5@gmail.com>
2024-04-29 22:34:56 +0530

    add annotations

--- src/main/java/com/socialfitness/socialfitness/models/Post.java
+++ src/main/java/com/socialfitness/socialfitness/models/Post.java
@@ -2,14 +2,19 @@ package com.socialfitness.socialfitness.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Post {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
@@ -23,82 +28,5 @@ public class Post {
     private List<User> liked = new ArrayList<>();
     @OneToMany
     private List<Comment> comments = new ArrayList<>();
-    public Post() {
 
-    }
-
-    public Post(Integer id, String caption, String image, String video, User user, LocalDateTime createdAt, List<User> liked, List<Comment> comments) {
-        this.id = id;
-        this.caption = caption;
-        this.image = image;
-        this.video = video;
-        this.user = user;
-        this.createdAt = createdAt;
-        this.liked = liked;
-        this.comments = comments;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCaption() {
-        return caption;
-    }
-
-    public void setCaption(String caption) {
-        this.caption = caption;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getVideo() {
-        return video;
-    }
-
-    public void setVideo(String video) {
-        this.video = video;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public List<User> getLiked() {
-        return liked;
-    }
-
-    public void setLiked(List<User> liked) {
-        this.liked = liked;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
 }
--- src/main/java/com/socialfitness/socialfitness/models/User.java
+++ src/main/java/com/socialfitness/socialfitness/models/User.java
@@ -1,11 +1,17 @@
 package com.socialfitness.socialfitness.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Table(name = "users")
 public class User {
 
@@ -19,93 +25,6 @@ public class User {
     private String gender;
     private List<Integer> followers = new ArrayList<>();
     private List<Integer> followings = new ArrayList<>();
-    @ManyToMany
-    private List<Post> savedPost = new ArrayList<>();
-    public User() {
+    private String proImage;
 
-    }
-
-    public User(Integer id, String firstName, String lastName, String email, String password, String gender, List<Integer> followers, List<Integer> followings, List<Post> savedPost) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.gender = gender;
-        this.followers = followers;
-        this.followings = followings;
-        this.savedPost = savedPost;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public List<Post> getSavedPost() {
-        return savedPost;
-    }
-
-    public void setSavedPost(List<Post> savedPost) {
-        this.savedPost = savedPost;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public List<Integer> getFollowers() {
-        return followers;
-    }
-
-    public void setFollowers(List<Integer> followers) {
-        this.followers = followers;
-    }
-
-    public List<Integer> getFollowings() {
-        return followings;
-    }
-
-    public void setFollowings(List<Integer> followings) {
-        this.followings = followings;
-    }
 }
